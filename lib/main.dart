03429cda9be376812b4607f2c849be4e66a94f56
Muhammad Umar Khan <64498330+UmarKhan72@users.noreply.github.com>
2021-06-28 14:02:09 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'todo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Todo-App"),
+          ),
+        ),
+        body: Todo(),
+      ),
+    );
+  }
+}
